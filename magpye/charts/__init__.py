58c3661c6ff6fa0d20aa1d519ee861281750284f
James Varndell <36902610+JamesVarndell@users.noreply.github.com>
2023-01-12 09:50:56 +0000

    Add chart skeleton (#3)
    * Tweaks to domain detection algorithm
    
    * Adds base Chart class
    
    * Adds docstrings for primary chart methods
    
    * Adds chart domain and CRS examples
    
    * Enforce cartopy version
    
    * Fixes CRS compatibility with custom domains
    
    * Redefines North Atlantic domain
    
    * Minor style tweaks
    
    * Adds more domain aliases
    
    * Adds tests for Chart class
    
    * Adds more Chart tests

---
+++ magpye/charts/__init__.py
@@ -0,0 +1,159 @@
+import cartopy.feature as cfeature
+import matplotlib.pyplot as plt
+
+from magpye.domains import auto
+from magpye.schema import schema
+
+
+def await_crs(method):
+    def wrapper(self, *args, **kwargs):
+        if self._crs is None:
+            self._queue.append((method, args, kwargs))
+        else:
+            return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+class Chart:
+    """
+    A geospatial chart containing a single axis.
+
+    Parameters
+    ----------
+    domain : str or list (optional)
+        Must be either:
+            - A string naming the domain to use for the chart (e.g. a country,
+              continent or state/province).
+            - A list containing the bounding box of the domain, in the order
+              `[min_x, max_x, min_y, max_y]`. The bounding box is assumed to be
+              defined in terms of latitude and longitude values, unless the
+              `domain_crs` argument is also passed, in which case the bounding
+              box must be defined using `domain_crs` coordinates.
+        If no domain is passed, the chart will attempt to use the best domain
+        to fit the first field of data plotted on the chart.
+    crs : cartopy.crs.CRS (optional)
+        The coordinate reference system to use for this chart. If `None`
+        (default), will attempt to find the best CRS to suit the bounding box
+        of the chart.
+    domain_crs : cartopy.crs.CRS (optional)
+        Use with `domain` to specify the coordinate system on which the domain
+        bounding box is defined.
+    figsize : list (optional)
+        The width and height (in inches) of the chart. Note that the width of
+        the chart may be adjusted slightly if the aspect ratio of your chart
+        does not match the figsize.
+    """
+
+    def __init__(self, domain=None, crs=None, domain_crs=None, figsize=(10, 10)):
+        self._domain = domain
+        self._crs = crs
+        self._domain_crs = domain_crs
+        self._bounds = None
+        self._boundless = False
+
+        self._queue = []
+
+        self._fig = None
+        self._figsize = figsize
+
+        self._axis = None
+
+        self._cbar_axes = None
+        self._cbar_locations = None
+
+        self._layers = dict()
+
+    @staticmethod
+    def close_current_fig():
+        plt.clf()
+        plt.close()
+
+    @property
+    def fig(self):
+        if self._fig is None:
+            self.close_current_fig()
+            self._fig = plt.figure(figsize=self._figsize)
+        return self._fig
+
+    @property
+    def axis(self):
+        if self._axis is None:
+            self._axis = self.fig.add_subplot(1, 1, 1, projection=self.crs)
+            if self.bounds is not None:
+                self._axis.set_extent(self.bounds, crs=self.crs)
+        return self._axis
+
+    @property
+    def bounds(self):
+        if self._bounds is None and not self._boundless:
+            self._setup_domain()
+        return self._bounds
+
+    @property
+    def crs(self):
+        if self._crs is None:
+            self._setup_domain()
+        return self._crs
+
+    def _setup_domain(self):
+        if self._bounds is None:
+            if isinstance(self._domain, (list, tuple)):
+                assert len(self._domain) == 4, (
+                    f"domains must contain 4 elements ([min_x, max_x, min_y, "
+                    f"max_y]); got {len(self._domain)}"
+                )
+                if self._crs is None:
+                    self._crs = auto.get_optimal_crs(self._domain)
+                self._bounds = auto.get_crs_extents(
+                    self._domain, self._crs, self._domain_crs or schema.reference_crs
+                )
+            elif isinstance(self._domain, str):
+                self._bounds, self._crs = auto.get_bounds_and_crs(
+                    self._domain, self._crs
+                )
+            elif self._domain is None:
+                self._crs = self._crs or schema.reference_crs
+                self._boundless = True
+            else:
+                raise ValueError(
+                    f"'domain' must be str or list; got {type(self._domain)}"
+                )
+
+    @await_crs
+    @schema.coastlines.apply()
+    def coastlines(self, **kwargs):
+        """Add coastal outlines from the Natural Earth “coastline” shapefile collection."""
+        return self.axis.coastlines(**kwargs)
+
+    @await_crs
+    @schema.borders.apply()
+    def borders(self, **kwargs):
+        """Add political boundaries from the Natural Earth administrative shapefile collection."""
+        return self.axis.add_feature(cfeature.BORDERS, **kwargs)
+
+    @await_crs
+    @schema.gridlines.apply()
+    def gridlines(self, **kwargs):
+        """Add latitude and longitude gridlines."""
+        return self.axis.gridlines(**kwargs)
+
+    @await_crs
+    def land(self, **kwargs):
+        """Add land polygons from the Natural Earth "land" shapefile collection."""
+        return self.axis.add_feature(cfeature.LAND, **kwargs)
+
+    def _release_queue(self):
+        while len(self._queue):
+            method, args, kwargs = self._queue.pop(0)
+            method(self, *args, **kwargs)
+
+    def show(self, *args, **kwargs):
+        """Display the chart."""
+        self._release_queue()
+        plt.show(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        """Save the chart."""
+        self._release_queue()
+        plt.savefig(*args, **kwargs)
---
+++ tests/charts/test_Chart.py
@@ -0,0 +1,106 @@
+import os
+from functools import partial
+from pathlib import Path
+
+import cartopy
+import matplotlib
+import pytest
+
+from magpye import charts, schema
+
+
+# TODO: There is a bug in matplotlib causing an incorrect DeprecationWarning
+# to be raised; ignore for now, but update when matplotlib has fixed this bug
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+def test_Chart_queue():
+    chart = charts.Chart()
+
+    assert len(chart._queue) == 0
+
+    chart.coastlines()
+    assert len(chart._queue) == 1
+    assert chart._queue[0][1] == tuple()
+    assert chart._queue[0][2] == dict()
+
+    chart._release_queue()
+    assert len(chart._queue) == 0
+
+
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+def test_Chart_bounds_and_crs():
+    chart = charts.Chart()
+    assert chart.bounds is None
+    assert chart.crs == schema.reference_crs
+
+    expected_bounds = [-363796, 373425, -541558, 545791]
+    expected_crs_params = {
+        "ellps": "WGS84",
+        "proj": "aea",
+        "lon_0": -2.9,
+        "lat_0": 54.3,
+        "x_0": 0.0,
+        "y_0": 0.0,
+        "lat_1": 51.3,
+        "lat_2": 57.2,
+    }
+    chart = charts.Chart(domain="United Kingdom")
+    assert chart.bounds == pytest.approx(expected_bounds, 0.5)
+    crs_params = chart.crs.proj4_params
+    for key in expected_crs_params:
+        if isinstance(crs_params[key], (int, float)):
+            assert crs_params[key] == pytest.approx(expected_crs_params[key], 1)
+        else:
+            assert crs_params[key] == expected_crs_params[key]
+
+
+@pytest.fixture
+def dummy_chart():
+    path = Path(__file__).parent / "temporary-chart.png"
+    chart = charts.Chart()
+    chart.temporary_save = partial(chart.save, path)
+    yield chart
+    os.remove(path)
+
+
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+def test_Chart_coastlines(dummy_chart):
+    basic_children = dummy_chart.axis.get_children()
+
+    dummy_chart.coastlines()
+    dummy_chart.temporary_save()
+    new_children = dummy_chart.axis.get_children()
+    assert len(new_children) == len(basic_children) + 1
+    assert isinstance(new_children[0], cartopy.mpl.feature_artist.FeatureArtist)
+
+
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+def test_Chart_gridlines(dummy_chart):
+    basic_children = dummy_chart.axis.get_children()
+
+    dummy_chart.gridlines()
+    dummy_chart.temporary_save()
+    new_children = dummy_chart.axis.get_children()
+    assert len(new_children) > len(basic_children)
+    assert isinstance(new_children[0], matplotlib.collections.LineCollection)
+
+
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+def test_Chart_borders(dummy_chart):
+    basic_children = dummy_chart.axis.get_children()
+
+    dummy_chart.borders()
+    dummy_chart.temporary_save()
+    new_children = dummy_chart.axis.get_children()
+    assert len(new_children) == len(basic_children) + 1
+    assert isinstance(new_children[0], cartopy.mpl.feature_artist.FeatureArtist)
+
+
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
+def test_Chart_land(dummy_chart):
+    basic_children = dummy_chart.axis.get_children()
+
+    dummy_chart.land()
+    dummy_chart.temporary_save()
+    new_children = dummy_chart.axis.get_children()
+    assert len(new_children) == len(basic_children) + 1
+    assert isinstance(new_children[0], cartopy.mpl.feature_artist.FeatureArtist)
