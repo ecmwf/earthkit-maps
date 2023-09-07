bf87f849fd1d9e8d6930006161a6ba4b2cc74c5e
James Varndell <james.varndell@ecmwf.int>
2023-09-07 07:53:49 +0100

    Fixes a bug which prevents automatic titles from working when data has no 'type' metadata

--- earthkit/maps/layers/layers.py
+++ earthkit/maps/layers/layers.py
@@ -73,7 +73,7 @@ class Layer:
 
     @property
     def _default_title_template(self):
-        if self.data.metadata("type", default=None) == "an":
+        if self.data.metadata("type", default="an") == "an":
             template = metadata.DEFAULT_ANALYSIS_TITLE
         else:
             template = metadata.DEFAULT_FORECAST_TITLE
