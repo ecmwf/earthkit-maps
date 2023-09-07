252186dc73ecd4797f29b61df62a8a6d01695878
James Varndell <james.varndell@ecmwf.int>
2023-09-07 11:50:44 +0100

    QA tweaks

--- earthkit/maps/domains/domain.py
+++ earthkit/maps/domains/domain.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import warnings
-
 import cartopy.crs as ccrs
 import cv2
 import numpy as np
