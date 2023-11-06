c7b4e2049e635dca7f2f8deee3443426dc1bb641
Harrison Cook <harrison.cook@bom.gov.au>
2023-11-06 20:49:52 +1100

    Adjust tcwv to match ECMWF Standards

--- earthkit/maps/styles/tcwv.py
+++ earthkit/maps/styles/tcwv.py
@@ -3,6 +3,6 @@ from earthkit.maps import styles
 TCWV_IN_KG_M2 = styles.Contour(
     colors="viridis",
     extend="both",
-    levels=range(0, 100, 5),
+    levels=range(0, 150, 10),
     units="kg m-2",
 )
