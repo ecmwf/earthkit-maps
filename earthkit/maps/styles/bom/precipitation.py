56f20e7538b13dc575dc783bc453f53d1aecce0d
Harrison Cook <harrison.cook@bom.gov.au>
2023-12-06 17:18:14 +1100

    Remove ECMWF Copyright from Bureau Styles - Seperate tcwv and precwtr

--- earthkit/maps/styles/bom/precipitation.py
+++ earthkit/maps/styles/bom/precipitation.py
@@ -1,6 +1,18 @@
+# Copyright 2023, Australian Bureau of Meteorology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from earthkit.maps import styles
-import numpy as np
 
 PRECIP_COLOURS =[
         (0.667, 1.0, 1.0),
--- earthkit/maps/styles/bom/tcwv.py
+++
@@ -1,8 +0,0 @@
-from earthkit.maps import styles
-
-TCWV_IN_KG_M2 = styles.Contour(
-    colors="viridis",
-    extend="both",
-    levels=range(0, 150, 10),
-    units="kg m-2",
-)
